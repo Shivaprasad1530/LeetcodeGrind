44d5667b38dba5ac92f5bf642e5abf26f8a3787c
Shivaprasad1530 <153277825+Shivaprasad1530@users.noreply.github.com>
2026-08-09 15:10:38 +0530

    Time: 9 ms (97.45%) | Memory: 46.3 MB (42.22%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -1,17 +1,20 @@
 class Solution {
     public int numberOfSubstrings(String s) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        map.put('a',-1);
-        map.put('b',-1);
-        map.put('c',-1);
-        int count=0;
-        for(int i=0;i<s.length();i++){
-            char c = s.charAt(i);
-            map.replace(c,i);
-            if(map.get('a')>-1 && map.get('b')>-1 && map.get('c')>-1){
-                int m = Math.min(map.get('a'),map.get('b'));
-                int n = Math.min(m,map.get('c'));
-                count += 1 + (n-0);
+        int n = s.length();
+        int lastA = -1;
+        int lastB = -1;
+        int lastC = -1;
+        int count = 0;
+        for(int i=0;i<n;i++){
+            char ch = s.charAt(i);
+            if(ch=='a') lastA = i;
+            else if(ch=='b') lastB = i;
+            else lastC = i;
+
+            int minIdx = Math.min(lastA, Math.min(lastB, lastC));
+
+            if(minIdx != -1){
+                count += minIdx + 1;
             }
         }
         return count;
